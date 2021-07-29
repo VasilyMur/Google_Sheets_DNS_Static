0354f0064123e36e3870bf657cd27d3ce6ecc37c
vasilymur <vmouravi@gmail.com>
2021-07-29 15:46:04 +0300

    testing - static pages removed

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,83 +5,84 @@ import { CartConsumer } from '../../components/Features/WebsiteFeatureThemeOne/C
 import { uniqueCategorySlugsFromCards } from '../../components/Features/WebsiteFeatureThemeOne/helpers';
 
 const Category = (props) => (
-  <CartConsumer>
-    {({ addProduct, removeProduct, products }) => (
-      <WebsiteSingleCategoryPageFeature
-        {...props}
-        addProduct={addProduct}
-        removeProduct={removeProduct}
-        products={products}
-      />
-    )}
-  </CartConsumer>
+  // <CartConsumer>
+  //   {({ addProduct, removeProduct, products }) => (
+  //     <WebsiteSingleCategoryPageFeature
+  //       {...props}
+  //       addProduct={addProduct}
+  //       removeProduct={removeProduct}
+  //       products={products}
+  //     />
+  //   )}
+  // </CartConsumer>
+  <p>CATEGORY PAGE</p>
 );
 
-export const getStaticPaths = async () => {
-  const response = await api.get(`${URL_API_GET_WEBSITE}`);
-  const { data } = response;
+// export const getStaticPaths = async () => {
+//   const response = await api.get(`${URL_API_GET_WEBSITE}`);
+//   const { data } = response;
 
-  if (!data || !Object.keys(data).length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!data || !Object.keys(data).length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const { cards } = data;
+//   const { cards } = data;
 
-  if (!cards || !cards.length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!cards || !cards.length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const categorySlugsMapped = uniqueCategorySlugsFromCards(cards);
-  const slugs = Object.values(categorySlugsMapped).map((res) => res.slug);
-  const paths = slugs.map((slug) => ({ params: { slug } }));
+//   const categorySlugsMapped = uniqueCategorySlugsFromCards(cards);
+//   const slugs = Object.values(categorySlugsMapped).map((res) => res.slug);
+//   const paths = slugs.map((slug) => ({ params: { slug } }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps = async ({ params: { slug } }) => {
-  const response = await api.get(`${URL_API_GET_WEBSITE}`);
-  const { data } = response;
+// export const getStaticProps = async ({ params: { slug } }) => {
+//   const response = await api.get(`${URL_API_GET_WEBSITE}`);
+//   const { data } = response;
 
-  if (!data || !Object.keys(data).length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!data || !Object.keys(data).length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const { cards } = data;
+//   const { cards } = data;
 
-  if (!cards || !cards.length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!cards || !cards.length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const allProductsWithCurrentFilter = cards.filter((singleCard) =>
-    singleCard.category.some((res) => res.slug === slug)
-  );
-  const categorySlugsMapped = uniqueCategorySlugsFromCards(cards);
-  const item = categorySlugsMapped[slug];
+//   const allProductsWithCurrentFilter = cards.filter((singleCard) =>
+//     singleCard.category.some((res) => res.slug === slug)
+//   );
+//   const categorySlugsMapped = uniqueCategorySlugsFromCards(cards);
+//   const item = categorySlugsMapped[slug];
 
-  if (!item || !Object.keys(item).length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!item || !Object.keys(item).length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  return {
-    props: {
-      data,
-      dataCategory: allProductsWithCurrentFilter,
-      currentCategory: item,
-    },
-  };
-};
+//   return {
+//     props: {
+//       data,
+//       dataCategory: allProductsWithCurrentFilter,
+//       currentCategory: item,
+//     },
+//   };
+// };
 
 export default Category;
--- pages/item/[slug].js
+++ pages/item/[slug].js
@@ -4,79 +4,80 @@ import { URL_API_GET_WEBSITE } from '../../constants';
 import { CartConsumer } from '../../components/Features/WebsiteFeatureThemeOne/CartProvider';
 
 const Item = (props) => (
-  <CartConsumer>
-    {({ addProduct, removeProduct, products }) => (
-      <WebsiteSinglePageFeature
-        {...props}
-        addProduct={addProduct}
-        removeProduct={removeProduct}
-        products={products}
-      />
-    )}
-  </CartConsumer>
+  // <CartConsumer>
+  //   {({ addProduct, removeProduct, products }) => (
+  //     <WebsiteSinglePageFeature
+  //       {...props}
+  //       addProduct={addProduct}
+  //       removeProduct={removeProduct}
+  //       products={products}
+  //     />
+  //   )}
+  // </CartConsumer>
+  <p>ITEM PAGE</p>
 );
 
-export const getStaticPaths = async () => {
-  const response = await api.get(`${URL_API_GET_WEBSITE}`);
-  const { data } = response;
+// export const getStaticPaths = async () => {
+//   const response = await api.get(`${URL_API_GET_WEBSITE}`);
+//   const { data } = response;
 
-  if (!data || !Object.keys(data).length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!data || !Object.keys(data).length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const { cards } = data;
+//   const { cards } = data;
 
-  if (!cards || !cards.length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!cards || !cards.length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const slugs = cards
-    .filter((card) => !card.link.includes('http'))
-    .map((res) => res.link);
-  const paths = slugs.map((slug) => ({ params: { slug } }));
+//   const slugs = cards
+//     .filter((card) => !card.link.includes('http'))
+//     .map((res) => res.link);
+//   const paths = slugs.map((slug) => ({ params: { slug } }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps = async ({ params: { slug } }) => {
-  const response = await api.get(`${URL_API_GET_WEBSITE}`);
-  const { data } = response;
+// export const getStaticProps = async ({ params: { slug } }) => {
+//   const response = await api.get(`${URL_API_GET_WEBSITE}`);
+//   const { data } = response;
 
-  if (!data || !Object.keys(data).length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!data || !Object.keys(data).length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const { cards } = data;
+//   const { cards } = data;
 
-  if (!cards || !cards.length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!cards || !cards.length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const item = cards.find((res) => res.link === slug);
+//   const item = cards.find((res) => res.link === slug);
 
-  if (!item || !Object.keys(item).length) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!item || !Object.keys(item).length) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  return {
-    props: {
-      data,
-      dataProduct: item,
-    },
-  };
-};
+//   return {
+//     props: {
+//       data,
+//       dataProduct: item,
+//     },
+//   };
+// };
 
 export default Item;
